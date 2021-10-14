d5fe451699ed4809e0003f1ed7e089d5e81b761a
ir-sen <irsen.kan@bk.ru>
2021-10-14 18:52:31 +0300

    Bind DetailActivity

--- app/src/main/java/com/sumin/mymovies/DetailActivity.java
+++ app/src/main/java/com/sumin/mymovies/DetailActivity.java
@@ -1,13 +1,62 @@
 package com.sumin.mymovies;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.sumin.mymovies.data.MainViewModel;
+import com.sumin.mymovies.data.Movie;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewBigPoster;
+    private TextView textViewTitle;
+    private TextView textViewOriginalTitle;
+    private TextView textViewRating;
+    private TextView textViewReleaseDate;
+    private TextView textViewOverview;
+
+    private int id;
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        imageViewBigPoster = findViewById(R.id.imageViewBigPoster);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewOriginalTitle = findViewById(R.id.textViewOriginalTitle);
+        textViewRating = findViewById(R.id.textViewRating);
+        textViewReleaseDate = findViewById(R.id.textViewRealiseDate);
+        textViewOverview = findViewById(R.id.textViewOverview);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("id")) {
+            id = intent.getIntExtra("id", -1);
+        } else {
+            finish();
+        }
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        Movie movie = viewModel.getMovieById(id);
+        Picasso.get().load(movie.getBigPosterPath()).into(imageViewBigPoster);
+        textViewTitle.setText(movie.getTitle());
+        textViewOriginalTitle.setText(movie.getOriginalTitle());
+        textViewOverview.setText(movie.getOverview());
+        textViewReleaseDate.setText(movie.getReleaseDate());
+        textViewRating.setText(Double.toString(movie.getVoteAverage()));
+
+
+
+    }
+
+    public void onClickChangeFavorite(View view) {
     }
 }
